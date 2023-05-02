859aa8073487651411c435366c14837dc91b50bd
Ivan Shlyakov <ivanski1903@icloud.com>
2023-05-03 01:02:03 +0800

    v2

--- scripts/index.js
+++ scripts/index.js
@@ -2,37 +2,31 @@ const popup = document.querySelector('.popup');
 const popupToggleBtn = document.querySelector('.profile__edit-button');
 const popupCloseBtn = document.querySelector('.popup__btn-close');
 const popupForm = document.querySelector('.popup__form');
-const userNameInput = document.querySelector('.popup__input_name');
-const userAboutInput = document.querySelector('.popup__input_about');
+const userNameInput = document.querySelector('.popup__input_type_name');
+const userAboutInput = document.querySelector('.popup__input_type_about');
 
 
 function openPopup() {
-  popup.classList.add('popup__open');
+  popup.classList.add('popup__opened');
+  userNameInput.value = profileName.textContent;
+  userAboutInput.value = profileAbout.textContent;
 }
 
 function closePopup() {
-  popup.classList.remove('popup__open');
+  popup.classList.remove('popup__opened');
 }
 
 popupToggleBtn.addEventListener('click', openPopup);
-
 popupCloseBtn.addEventListener('click', closePopup);
 
 const profileName = document.querySelector('.profile__name');
 const profileAbout = document.querySelector('.profile__about');
 
-const nameInput = document.querySelector('.popup__input_name');
-const aboutInput = document.querySelector('.popup__input_about');
-
-nameInput.value = profileName.textContent;
-aboutInput.value = profileAbout.textContent;
-
-
 function handleFormSubmit (evt) {
     evt.preventDefault(); 
 
-    profileName.textContent = nameInput.value;
-    profileAbout.textContent = aboutInput.value;
+    profileName.textContent = userNameInput.value;
+    profileAbout.textContent = userAboutInput.value;
 
     closePopup();
 }
